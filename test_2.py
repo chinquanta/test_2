e096cececd9676ac228100dca803e8c7ae4d61ba
bagira <chinquanta2014@gmail.com>
2022-10-05 11:32:19 +0300

    переименовал основной файл в test_2

--- test_2.py
+++ test_2.py
@@ -1,30 +1,27 @@
 import csv
 
 
-def read_file(file_name):
-    '''
-    Считывает файл с данными
-    '''
+def read_file(file_name: str) -> list:
+    """
+    Считывает файл с данными в список строк.
+    """
     with open(file_name, 'r', newline='') as csvfile:
-        df = csv.reader(csvfile, delimiter=';')
-        db = list(df)
-        features_ix = {feature: db[0].index(feature) for feature in db[0]}
-        features = {feature: [row[ix] for row in db[1:]] for feature, ix in features_ix.items()}
-        features['Оклад'] = list(map(int, features['Оклад']))
-    return features
+        db = csv.reader(csvfile, delimiter=';')
+        db = list(db)
+        return db
 
 
-def menu(file_name):
-    '''
+def menu(file_name: str):
+    """
     Предоставляет интерфейс для управления
     набором опций по обработке данных.
-    '''
+    """
     features = read_file(file_name)
     print('Что нужно сделать:\n')
     print('Вывести в понятном виде иерархию команд,\n'
           'т.е. департамент и все команды, которые входят в него -', 1, '\n')
-    print('Вывести сводный отчёт по департаментам:\n'
-          'название, численность, "вилка" зарплат в виде мин – макс, среднюю зарплату -', 2, '\n')
+    print('Вывести сводный отчёт по департаментам:\n название, численность,'
+          '"вилка" зарплат в виде мин – макс, среднюю зарплату -', 2, '\n')
     print('Сохранить сводный отчёт из предыдущего пункта в виде csv-файла.\n'
           'При этом необязательно вызывать сначала команду из п.2 -', 3, '\n')
     print('Закончить - 4')
@@ -46,33 +43,37 @@ def menu(file_name):
             print('Выберите: 1, 2, 3 или 4')
 
 
-def struct_depts(features):
-    '''
+def struct_depts(db: list):
+    """
     Формирует структуру департаментов и печатает ее
-    '''
-    set_depts = set(features['Департамент'])
-    set_pairs = set(zip(features['Департамент'], features['Отдел']))
-    dict_struct = dict()
-    for dept in set_depts:
-        print(dept)
-        flr = filter(lambda x: x[0] == dept, set_pairs)
-        dict_struct[dept] = [item[1] for item in flr]
-        [print('    ' + x) for x in dict_struct[dept]]
-    return
+    """
+    columns = db[0]
+    data = db[1:]
+    ix_dep, ix_sub = [columns.index('Департамент'), columns.index('Отдел')]
+    struct_set = {(x[ix_dep], x[ix_sub]) for x in data}
+    struct_set = sorted(struct_set, key = lambda x: x[0])
+    dep = ''
+    for x in struct_set:
+        if x[0] != dep:
+            print(x[0])
+            dep = x[0]
+        print('    ' + x[1])
 
 
-def struct_salary(features, key):
-    '''
-    Формирует таблицу сводного отчета по департаментам.+
+def struct_salary(db: list, key: str):
+    """
+    Формирует таблицу сводного отчета по департаментам.
     Если key = 2, то печатает ее,
     если key = 3, то записывает ее в файл.
-    '''
-    set_depts = set(features['Департамент'])
-    list_pairs = list(zip(features['Департамент'], features['Оклад']))
+    """
+    columns = db[0]
+    data = db[1:]
+    ix_dep, ix_sal = [columns.index('Департамент'), columns.index('Оклад')]
+    set_depts = {x[ix_dep] for x in data}
     dict_struct = dict()
     for dept in set_depts:
-        flr = filter(lambda x: x[0] == dept, list_pairs)
-        dict_struct[dept] = [item[1] for item in flr]
+        flr = filter(lambda x: x[ix_dep] == dept, db)
+        dict_struct[dept] = [int(item[ix_sal]) for item in flr]
         dict_struct[dept] = [len(dict_struct[dept]), min(dict_struct[dept]),
                             sum(dict_struct[dept]) / len(dict_struct[dept]),
                             max(dict_struct[dept])]
@@ -86,7 +87,6 @@ def struct_salary(features, key):
         print('Департ.', 'числ-ть', 'мин.', 'ср.знач.', 'макс.')
         for dept in set_depts:
             print(dept, *dict_struct[dept])
-    return
 
 
 if __name__ == '__main__':
--- test_2_by_cols.py
+++ test_2_by_cols.py
@@ -1,17 +1,20 @@
 import csv
 
 
-def read_file(file_name: str) -> list:
+def read_file(file_name):
     '''
-    Считывает файл с данными в список строк.
+    Считывает файл с данными
     '''
     with open(file_name, 'r', newline='') as csvfile:
         df = csv.reader(csvfile, delimiter=';')
         db = list(df)
-        return db
+        features_ix = {feature: db[0].index(feature) for feature in db[0]}
+        features = {feature: [row[ix] for row in db[1:]] for feature, ix in features_ix.items()}
+        features['Оклад'] = list(map(int, features['Оклад']))
+    return features
 
 
-def menu(file_name: str):
+def menu(file_name):
     '''
     Предоставляет интерфейс для управления
     набором опций по обработке данных.
@@ -20,8 +23,8 @@ def menu(file_name: str):
     print('Что нужно сделать:\n')
     print('Вывести в понятном виде иерархию команд,\n'
           'т.е. департамент и все команды, которые входят в него -', 1, '\n')
-    print('Вывести сводный отчёт по департаментам:\n название, численность,'
-          '"вилка" зарплат в виде мин – макс, среднюю зарплату -', 2, '\n')
+    print('Вывести сводный отчёт по департаментам:\n'
+          'название, численность, "вилка" зарплат в виде мин – макс, среднюю зарплату -', 2, '\n')
     print('Сохранить сводный отчёт из предыдущего пункта в виде csv-файла.\n'
           'При этом необязательно вызывать сначала команду из п.2 -', 3, '\n')
     print('Закончить - 4')
@@ -41,41 +44,35 @@ def menu(file_name: str):
             return
         else:
             print('Выберите: 1, 2, 3 или 4')
-    return
 
 
-def struct_depts(df: list):
+def struct_depts(features):
     '''
     Формирует структуру департаментов и печатает ее
     '''
-    columns = df[0]
-    data = df[1:]
-    ix_dep, ix_sub = [columns.index('Департамент'), columns.index('Отдел')]
-    struct_set = {(x[ix_dep], x[ix_sub]) for x in data}
-    struct_set = sorted(struct_set, key = lambda x: x[0])
-    dep = ''
-    for x in struct_set:
-        if x[0] != dep:
-            print(x[0])
-            dep = x[0]
-        print('    ' + x[1])
+    set_depts = set(features['Департамент'])
+    set_pairs = set(zip(features['Департамент'], features['Отдел']))
+    dict_struct = dict()
+    for dept in set_depts:
+        print(dept)
+        flr = filter(lambda x: x[0] == dept, set_pairs)
+        dict_struct[dept] = [item[1] for item in flr]
+        [print('    ' + x) for x in dict_struct[dept]]
     return
 
 
-def struct_salary(df: list, key: str):
+def struct_salary(features, key):
     '''
-    Формирует таблицу сводного отчета по департаментам.
+    Формирует таблицу сводного отчета по департаментам.+
     Если key = 2, то печатает ее,
     если key = 3, то записывает ее в файл.
     '''
-    columns = df[0]
-    data = df[1:]
-    ix_dep, ix_sal = [columns.index('Департамент'), columns.index('Оклад')]
-    set_depts = {x[ix_dep] for x in data}
+    set_depts = set(features['Департамент'])
+    list_pairs = list(zip(features['Департамент'], features['Оклад']))
     dict_struct = dict()
     for dept in set_depts:
-        flr = filter(lambda x: x[ix_dep] == dept, df)
-        dict_struct[dept] = [int(item[ix_sal]) for item in flr]
+        flr = filter(lambda x: x[0] == dept, list_pairs)
+        dict_struct[dept] = [item[1] for item in flr]
         dict_struct[dept] = [len(dict_struct[dept]), min(dict_struct[dept]),
                             sum(dict_struct[dept]) / len(dict_struct[dept]),
                             max(dict_struct[dept])]
